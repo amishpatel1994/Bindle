97e63fb7a2c92d7d71f7ec4cf16b742b395ad264
Stuart Watt <stuart@morungos.com>
2014-06-30 16:37:06 -0400

    Comment refactoring only. Nothing functional. Move along please

--- templates/server_setup_scripts/ubuntu_12.04_distributed_file_service.sh
+++ templates/server_setup_scripts/ubuntu_12.04_distributed_file_service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -vx
 
-# now setup volumes for use with gluster
+# now setup volumes for use with whatever distributed file system is to be used
 echo '%{HOSTS}' > /vagrant/distributed_file_hosts.txt
 perl /vagrant/setup_%{DISTRIBUTED_FILE_SYSTEM}_service.pl --host /vagrant/distributed_file_hosts.txt --dir-map /vagrant/distributed_file_volumes_report.txt
 
